c3f1a2455c8d5687d2b28f9da6b2457911fb0c39
Mario Berni <mariokpo.mb@gmail.com>
2025-05-12 10:48:09 -0300

    Initial project structure and basic main.dart

---
+++ android/app/src/main/kotlin/com/example/app_agenda_glam/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_agenda_glam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Agenda Glam',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Bienvenido a Agenda Glam'),
+        ),
+      ),
+    );
+  }
+}
